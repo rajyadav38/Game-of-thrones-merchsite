94a9f2515d680f6a1f24de3a871bf25f8da65786
rajyadav38 <rajyadav5566788@gmail.com>
2026-05-20 13:09:07 +0530

    production cors fix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://game-of-thrones-merchsite-ly6f.vercel.app/login",
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
