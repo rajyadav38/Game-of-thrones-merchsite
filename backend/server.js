74e7789193f6eb2d1ed4f689fe1b559817c65980
rajyadav38 <rajyadav5566788@gmail.com>
2026-05-20 13:31:17 +0530

    fix cors origin

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,48 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
 const contactRoutes = require("./routes/contactRoutes");
+
 dotenv.config();
 
 const app = express();
 
+// CORS
 app.use(
   cors({
-    origin: "https://game-of-thrones-merchsite-ly6f.vercel.app/login",
+    origin: "https://game-of-thrones-merchsite-ly6f.vercel.app",
   }),
 );
+
+// MIDDLEWARE
 app.use(express.json());
 
+// ROUTES
 app.use("/api/auth", require("./routes/authRoutes"));
+
 app.use("/api/products", require("./routes/productRoutes"));
+
 app.use("/api/orders", require("./routes/orderRoutes"));
+
 app.use("/api/contact", contactRoutes);
+
+// MONGODB CONNECTION
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
 
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+
+  .catch((err) => {
+    console.log("Mongo Error:", err.message);
+  });
+
+// PORT
 const PORT = process.env.PORT || 5000;
 
+// SERVER
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
